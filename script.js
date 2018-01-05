44bb1731282bf55eb85f33aaad6909ce030f37d1
Justin M <mckennaman123@users.noreply.github.com>
2018-01-04 19:50:52 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
+/*scroll button*/
 $(document).ready(function(){
     
-    function scrollToAnchor(aid){
-    var aTag = $("a[name='"+ aid +"']");
-    $('html,body').animate({scrollTop: aTag.offset().top},'slow');
+    function scroll_to_anchor(aid){
+    var a_tag = $("a[name='"+ aid +"']");
+    $('html,body').animate({scrollTop: a_tag.offset().top},'slow');
     }
 
-    $("#scrollButton").click(function() {
-   scrollToAnchor('scrollPoint');
+    $("#scroll_button").click(function() {
+   scroll_to_anchor('scroll_point');
     });
-
 });
